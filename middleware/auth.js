da1f3810decc9165f37bbe7f174dc71691ce7c35
ShameemSaifudeen <shameemsaif456@gmail.com>
2023-08-04 16:01:45 +0530

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,11 @@ function isAuthenticated(req, res, next) {
     if (req.session && req.session.userId) {
         next(); 
     } else {
-        res.redirect('/login');
+        // res.redirect('/login');
+
+
+
+        next()
     }
 }
 export function redirectIfAuthenticated(req, res, next) {
